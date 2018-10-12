b5a6e21e21207fa76adf9a64d762317b6dab6b4a
Chris Atwood <coachchris289@gmail.com>
2018-10-11 19:59:43 -0700

    user auth

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 export default ({currentUser}) => {
   // debugger;
+  // {<h1> Hello, {currentUser.username} </h1>}
   return (
   <div className="home">
     <h1> DOTIFY HOME PAGE</h1>
 
 
-        <h1> Hello, {currentUser.username} </h1>
+        <h1> Hello, You're on the home screen </h1>
 
   </div>
 );
