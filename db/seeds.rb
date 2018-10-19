38be4e3cf4b8b23ed301d2dcb6f05b8220acfdee
Chris Atwood <coachchris289@gmail.com>
2018-10-19 07:51:29 -0700

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,9 @@ City_of_Stars = EzDownload.open('https://s3-us-west-1.amazonaws.com/dotify-song-
 Someone_In_The_Crowd = EzDownload.open('https://s3-us-west-1.amazonaws.com/dotify-song-dev/Someone_In_The_Crowd.m4a')
 Start_A_Fire = EzDownload.open('https://s3-us-west-1.amazonaws.com/dotify-song-dev/Start_a_Fire.m4a')
 
-Song.create(title: "Whiplash",album_id:1).track.attach(io: Whiplash, filename: "Whiplash.m4a")
-Song.create(title: "Caravan",album_id:1).track.attach(io: Caravan, filename: 'Caravan.m4a' )
-Song.create(title: "Casey_song",album_id:1).track.attach(io:  Casey_song, filename: 'Casey_song.m4a'  )
+Song.create(title: "Whiplash",album_id:3).track.attach(io: Whiplash, filename: "Whiplash.m4a")
+Song.create(title: "Caravan",album_id:3).track.attach(io: Caravan, filename: 'Caravan.m4a' )
+Song.create(title: "Casey_song",album_id:3).track.attach(io:  Casey_song, filename: 'Casey_song.m4a'  )
 
 Song.create(title: "A Lovely Night", album_id:2).track.attach(io: A_lovely_night , filename: 'A_lovely_night.m4a'  )
 Song.create(title: "Another Day of Sun", album_id:2).track.attach(io:  Another_Day_of_Sun, filename: 'Another_Day_of_Sun.m4a' )
@@ -35,15 +35,15 @@ Song.create(title: "Start a Fire", album_id:2).track.attach(io: Start_A_Fire, fi
 
 
 
-Album.destroy_all
-
-Whiplash_cover = EzDownload.open('https://s3-us-west-1.amazonaws.com/dotify-song-dev/whiplash_cover.jpg')
-La_La_Land_cover = EzDownload.open('https://s3-us-west-1.amazonaws.com/dotify-song-dev/La_La_Land_cover.jpg')
-
-
-
-Album.create(title: "Whiplash_soundtrack", artist_id: 0).album_cover.attach(io: Whiplash_cover, filename: 'Whiplash_cover.jpg' )
-Album.create(title: "La La Land", artist_id: 0).album_cover.attach(io: La_La_Land_cover, filename: 'La_La_Land_cover.jp1' )
+# Album.destroy_all
+#
+# Whiplash_cover = EzDownload.open('https://s3-us-west-1.amazonaws.com/dotify-song-dev/whiplash_cover.jpg')
+# La_La_Land_cover = EzDownload.open('https://s3-us-west-1.amazonaws.com/dotify-song-dev/La_La_Land_cover.jpg')
+#
+#
+#
+# Album.create(title: "Whiplash_soundtrack", artist_id: 0).album_cover.attach(io: Whiplash_cover, filename: 'Whiplash_cover.jpg' )
+# Album.create(title: "La La Land", artist_id: 0).album_cover.attach(io: La_La_Land_cover, filename: 'La_La_Land_cover.jp1' )
 
 #
 # Song.destroy_all
