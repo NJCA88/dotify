46f0af35021e720f06f0309b7133819ee04a0bae
Chris Atwood <coachchris289@gmail.com>
2019-01-17 15:25:07 -0800

    continue progress on about page

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -15,6 +15,14 @@ class AboutComponent extends React.Component {
         return (
             <div className="about">
                 <h1> About the Developer</h1>
+
+                <div className = "aboutList">
+                <p> PICTURE OF ME</p>
+                <p>words words words about me</p>
+                    <button >GitHub</button>
+                    <button>LinkedIn</button>
+                    <button>Portfolio Site</button>
+                </div>
             </div>
         );
     }
