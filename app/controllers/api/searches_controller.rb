1465ddbcfcc71a361b768ca5d9a6083e1805d759
Chris Atwood <coachchris289@gmail.com>
2019-03-07 14:14:43 -0800

    get info flowing correctly via search

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,17 +1,26 @@
 require 'byebug'
 class Api::SearchesController < ApplicationController
+    protect_from_forgery with: :null_session
+
 
   def index
     @Albums = Album.all
     debugger
-    p "SEARCHING FOR SUPERMAN"
+    p "SEARCHING FOR BATMAN"
     render 'index'
   end
 
   def create
-    @Albums = Album.all
+    # @Albums = Album.all
+    search_string = request.body.read
+
+    artist = Artist.where(name: params[:search])
     debugger
-    p "SEARCHING FOR SUPERMAN"
+
+
+    # @Albums = []
+
+
     render 'index'
   end
 end
--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -4,37 +4,38 @@ class SearchComponent extends React.Component{
     constructor(props){
         super(props)
         this.submit = this.submit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.state = {value: ""}
     }
 
-    submit(){
-              console.log("submitting!");
-              fetch("/api/searches", {
-                method: "POST",
-                  body: JSON.stringify("hi there!!!"), // data can be `string` or {object}!
-              })
-                  .then(function (response) {
-                      return response.json();
-                  })
-                  .then(function (myJson) {
-                      console.log("THINGS: ", JSON.stringify(myJson));
-                  });
+    handleChange(event){
+        // console.log("chaning: ", this.state.value)
+        this.setState({ value: event.target.value });    
+    }
 
-            //   console.log("did anything happen?");
-
-              // fetch('http://example.com/movies.json')
-              //     .then(function (response) {
-              //         return response.json();
-              //     })
-              //     .then(function (myJson) {
-              //         console.log(JSON.stringify(myJson));
-              //     });
-            }
+    submit(e, value){
+            console.log("submitting!");
+            console.log("value is: ", this.state.value)
+            fetch("/api/searches", {
+            method: "POST",
+                body: JSON.stringify({search: this.state.value}), // data can be `string` or {object}!
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (myJson) {
+                    console.log("THINGS: ", JSON.stringify(myJson));
+                });
+    }
     render(){
         return(
             <div> 
                 <div className="searchPage">
                 <form onSubmit={this.submit}> 
-                    <input className="searchInput" type="text" ></input>
+                    <input className="searchInput" type="text" value={this.state.value} onChange={this.handleChange} ></input>
                 </form>
                 </div>
             </div>
@@ -42,3 +43,7 @@ class SearchComponent extends React.Component{
     }
 }
 export default SearchComponent;
+
+// <button onClick={e => {
+//     this.updateSongCollection(e, album, album.id);
+// }}
